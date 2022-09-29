8c0f23773d54ccaaff47583ffe3a5403ebd2aaed
Srimathi Elango <86002860+SrimathiElango21@users.noreply.github.com>
2022-09-29 21:33:37 +0530

    Phase 5 Practice Project 1

---
+++ Automate-a-Web-Application-1/src/test/java/LoginAutomation.java
@@ -0,0 +1,32 @@
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginAutomation {
+  @Test
+
+  public void login() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\chromedriver_win32/chromedriver.exe");
+	  
+	        WebDriver driver = new ChromeDriver();
+	        
+	        driver.get("https://www.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjY0MzY3NDQ2LCJjYWxsc2l0ZV9pZCI6MjY5NTQ4NDUzMDcyMDk1MX0%3D");
+	        
+	        driver.manage().window().maximize();
+	        
+	        driver.findElement(By.id("email"))
+	        .sendKeys("sri21@gmail.com");
+	        
+	        driver.findElement(By.id("pass"))
+	        .sendKeys("Srimathi@2001");
+	        
+	        driver.findElement(By.id("loginbutton"))
+          .click();
+  }
+}
